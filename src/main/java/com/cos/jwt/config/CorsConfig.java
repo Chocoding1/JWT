51e4b9e31304abb4c652839d5647b2a896e24d51
chocoding <galmeagi2@naver.com>
2025-03-17 15:31:46 +0900

    주석 추가

--- src/main/java/com/cos/jwt/config/CorsConfig.java
+++ src/main/java/com/cos/jwt/config/CorsConfig.java
@@ -11,13 +11,13 @@ public class CorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(); // 여러 URL 경로에 대해 각기 다른 CORS 설정을 적용할 수 있는 객체
+        CorsConfiguration config = new CorsConfiguration(); // CORS 정책을 설정하는 객체(허용할 출처, 헤더, HTTP 메서드 등을 정의)
         config.setAllowCredentials(true); // 내 서버가 응답을 할 때, json을 자바스크립트에서 처리하도록 설정
         config.addAllowedOrigin("*"); // 모든 ip에 응답을 허용
         config.addAllowedHeader("*"); // 모든 header에 응답을 허용
         config.addAllowedMethod("*"); // 모든 POST, GET, PUT, DELETE, FETCH에 요청을 허용
-        source.registerCorsConfiguration("/api/**", config);
+        source.registerCorsConfiguration("/api/**", config); // /api/** 경로에 대해 config에서 설정한 CORS 정책을 적용
         return new CorsFilter(source);
     }
 }
--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -21,13 +21,29 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /**
+         * JWT(JSON Web Token) 기반 인증을 사용할 때는 CSRF 보호가 필요 X
+         * CSRF는 세션 기반 인증(쿠키 기반)에서 주로 문제가 되는데, JWT는 쿠키를 사용하지 않으므로 비활성화
+         */
         http.csrf(CsrfConfigurer::disable);
         http
                 .addFilter(corsFilter)
+                /**
+                 * 세션을 사용하지 않도록 설정
+                 * JWT를 사용하면 세션을 저장할 필요가 없기 때문에 STATELESS(무상태)로 설정
+                 */
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+                /**
+                 * 기본적인 로그인 폼 기능 비활성화
+                 * JWT를 사용하므로 로그인 시 폼을 사용하지 않기 때문
+                 */
                 .formLogin(FormLoginConfigurer::disable)
+                /**
+                 * HTTP Basic 인증은 Authorization 헤더에 ID/PW를 담아 요청하는 방식
+                 * 보안이 취약하므로 JWT를 사용하면 필요 X
+                 */
                 .httpBasic(HttpBasicConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/v1/user/**").hasAnyRole("USER", "MANAGER", "ADMIN")
